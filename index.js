e85e950c2be7ec41cbb39b387a0714d5f219c827
Theo Pack <tf.pack@gmail.com>
2015-04-26 11:11:01 +0200

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ 
+  host: 'localhost', 
+  port: 8000 
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+  	var stars = Math.floor(Math.random() * 6);
+  	var review_count = Math.floor((Math.random() * 380) + 20);
+  	reply({
+  		id: encodeURIComponent(request.params.name),
+  		stars: stars,
+  		review_count: review_count
+  	});
+  }
+});
+
+server.start();
